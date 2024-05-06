83cb20882c987dc5e86edebfe9034e1f43834b6b
MESKINE FATIMA EZZAHRA <89028429+Fzmes@users.noreply.github.com>
2024-05-06 10:36:06 +0100

    6-

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,17 +3,12 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
 	try {
-		console.log("Request cookies:", req.cookies); // Log cookies for debugging
 		const token = req.cookies.jwt;
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
 		}
 
-		const tokenParts = token.split(' ');
-		if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-			return res.status(401).json({ error: "Unauthorized: Invalid Token Format" });
-		}
-		const decoded = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
@@ -28,7 +23,7 @@ export const protectRoute = async (req, res, next) => {
 		req.user = user;
 		next();
 	} catch (err) {
-		console.log("Error in protectRoute middleware:", err.message);
+		console.log("Error in protectRoute middleware", err.message);
 		return res.status(500).json({ error: "Internal Server Error" });
 	}
 };

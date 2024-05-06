bc9936dbfe7c86de8d48a9b4a5b47412e411bc2b
MESKINE FATIMA EZZAHRA <89028429+Fzmes@users.noreply.github.com>
2024-05-06 11:05:30 +0100

    7-

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,19 +3,22 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
 	try {
-		const token = req.cookies.jwt;
+		const authHeader = req.headers.authorization;
+		if (!authHeader) {
+			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
+		}
+
+		const token = authHeader.split(" ")[1];
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
 		}
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
 		}
 
 		const user = await User.findById(decoded.userId).select("-password");
-
 		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}

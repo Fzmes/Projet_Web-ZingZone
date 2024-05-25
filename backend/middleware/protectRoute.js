93b2a381241d47126c176af687b8f8c49ff5eec9
MESKINE FATIMA EZZAHRA <89028429+Fzmes@users.noreply.github.com>
2024-05-25 23:08:26 +0100

    commit_29

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -5,7 +5,6 @@ export const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
 		//const token = req.headers.authorization?.split(' ')[1];
-
         if (!token) {
             return res.status(401).json({ error: "Unauthorized: No Token Provided" });
         }
@@ -30,35 +29,3 @@ export const protectRoute = async (req, res, next) => {
 		return res.status(500).json({ error: "Internal Server Error" });
 	}
 };
-// import User from "../models/user.model.js";
-// import jwt from "jsonwebtoken";
-
-// export const protectRoute = async (req, res, next) => {
-// 	try {
-// 		const authHeader = req.headers.authorization;
-// 		if (!authHeader) {
-// 			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
-// 		}
-
-// 		const token = authHeader.split(" ")[1];
-// 		if (!token) {
-// 			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
-// 		}
-
-// 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-// 		if (!decoded) {
-// 			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
-// 		}
-
-// 		const user = await User.findById(decoded.userId).select("-password");
-// 		if (!user) {
-// 			return res.status(404).json({ error: "User not found" });
-// 		}
-
-// 		req.user = user;
-// 		next();
-// 	} catch (err) {
-// 		console.log("Error in protectRoute middleware", err.message);
-// 		return res.status(500).json({ error: "Internal Server Error" });
-// 	}
-// };
